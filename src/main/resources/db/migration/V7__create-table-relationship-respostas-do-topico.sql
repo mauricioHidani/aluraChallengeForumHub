dce06dff749e002101fc945f9321e00e3de8117d
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2024-12-26 18:08:58 -0300

    refactor: migration topicos and respostas id type

--- src/main/resources/db/migration/V7__create-table-relationship-respostas-do-topico.sql
+++ src/main/resources/db/migration/V7__create-table-relationship-respostas-do-topico.sql
@@ -1,6 +1,6 @@
 CREATE TABLE respostas_do_topico(
-    topico_id BINARY(16) NOT NULL,
-    resposta_id BINARY(16) NOT NULL,
+    topico_id BIGINT NOT NULL,
+    resposta_id BIGINT NOT NULL,
 
     PRIMARY KEY (topico_id, resposta_id),
     CONSTRAINT fk_respostas_do_topico_topico FOREIGN KEY (topico_id) REFERENCES topicos(id),
