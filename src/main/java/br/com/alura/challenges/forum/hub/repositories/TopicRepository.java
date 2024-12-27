6e6d9e4c1586f9eed601f6b08fea75968a70f75b
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2024-12-27 18:28:21 -0300

    feat: add find first 10 order by creation date asc

--- src/main/java/br/com/alura/challenges/forum/hub/repositories/TopicRepository.java
+++ src/main/java/br/com/alura/challenges/forum/hub/repositories/TopicRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, UUID> {
+
+    List<Topic> findTop10ByOrderByCreationDateAsc();
 
     Boolean existsByTitleAndMessage(String title, String message);
 
