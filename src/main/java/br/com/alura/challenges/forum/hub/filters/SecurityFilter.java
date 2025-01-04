7fb10f43fba34050c32c0a8776341b0d1b55a9fb
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2025-01-04 08:33:55 -0300

    feat: add security filter

---
+++ src/main/java/br/com/alura/challenges/forum/hub/filters/SecurityFilter.java
@@ -0,0 +1,54 @@
+package br.com.alura.challenges.forum.hub.filters;
+
+import br.com.alura.challenges.forum.hub.repositories.UserRepository;
+import br.com.alura.challenges.forum.hub.services.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    public SecurityFilter(final TokenService tokenService,
+                          final UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        final var tokenJWT = requestToken(request);
+
+        if (tokenJWT != null) {
+            final var subject = tokenService.getSubject(tokenJWT);
+            final var user = userRepository.findByEmail(subject).get();
+
+            final var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String requestToken(HttpServletRequest request) {
+        final var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+
+}
