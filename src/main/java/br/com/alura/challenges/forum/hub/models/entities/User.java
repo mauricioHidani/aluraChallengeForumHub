19bec41b365b742c8a80c698af399a1ed2c3ffe4
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2025-01-04 10:52:38 -0300

    feat: add constructor

--- src/main/java/br/com/alura/challenges/forum/hub/models/entities/User.java
+++ src/main/java/br/com/alura/challenges/forum/hub/models/entities/User.java
@@ -61,6 +61,13 @@ public class User implements UserDetails {
         this.password = password;
     }
 
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
     public Long getId() {
         return id;
     }
