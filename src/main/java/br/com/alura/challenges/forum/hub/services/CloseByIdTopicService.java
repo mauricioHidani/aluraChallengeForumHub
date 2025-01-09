ab8e87c062ef8d89e042a1e5fe11f7ce2996d923
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2025-01-09 08:22:21 -0300

    refactor: add auth by check username logged close

--- src/main/java/br/com/alura/challenges/forum/hub/services/CloseByIdTopicService.java
+++ src/main/java/br/com/alura/challenges/forum/hub/services/CloseByIdTopicService.java
@@ -1,8 +1,10 @@
 package br.com.alura.challenges.forum.hub.services;
 
 import br.com.alura.challenges.forum.hub.exceptions.NotFoundException;
+import br.com.alura.challenges.forum.hub.exceptions.UnauthorizedRequisitionException;
 import br.com.alura.challenges.forum.hub.models.enums.TopicStatus;
 import br.com.alura.challenges.forum.hub.repositories.TopicRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,12 +18,20 @@ public class CloseByIdTopicService {
     }
 
     @Transactional
-    public String execute(Long id) {
+    public String execute(Long id, Authentication authentication) {
+        final var authUsername = authentication.getName();
         final var closeStatus = TopicStatus.CLOSED;
         var found = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException(
                         "Tópico não encontrado pelo identificador especificado."
                 ));
+
+        final var author = found.getAuthor();
+        if (author == null || !author.getUsername().equals(authUsername)) {
+            throw new UnauthorizedRequisitionException(
+                "Não permitido que uma pessoa que não seja o autor modifique o tópico."
+            );
+        }
 
         found.setStatus(closeStatus);
         return closeStatus.getDescription();
